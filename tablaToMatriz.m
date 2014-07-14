b134e5c9d0f5dd4cd1f7a5ad9e19ed201ebc3134
Ignacio Tartavull <tartavull@gmail.com>
2014-07-14 20:52:52 -0300

    exam added

---
+++ tablaToMatriz.m
@@ -0,0 +1,11 @@
+function tablaToMatriz( tablaPrecedencias )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+   for index= 1:length(tablaPrecedencias)
+       
+       tablaPrecedencias{index,2}
+   end
+
+end
+
